acb803c3242fc2a8887fd855c076fc687d9cb910
VincentMugondora <vinmugondora@gmail.com>
2025-03-21 11:46:47 +0200

    doing files

--- testing.py
+++ testing.py
@@ -45,3 +45,34 @@ f = open("main.py", "rt")
 print(f.read())
 
 f.close()
+
+# Read Only Parts of the File
+# By default the read() method returns the whole text, but you can also specify how many characters you want to return:
+
+# Example
+# Return the 5 first characters of the file:
+print()
+print("Reading Five first characters")
+f = open("main.py", "r")
+print(f.read(5))
+
+
+print()
+# Read Lines
+# You can return one line by using the readline() method:
+
+# Example
+# Read one line of the file:
+print("Read one line of the file:")
+f = open("main.py", "r")
+print(f.readline())
+
+print()
+# By calling readline() two times, you can read the two first lines:
+
+# Example
+# Read two lines of the file:
+print("Read two lines of the file:")
+f = open("main.py", "r")
+print(f.readline())
+print(f.readline())
