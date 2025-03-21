1b5762a899dc78a1d5cf77e190627c9a0470a379
VincentMugondora <vinmugondora@gmail.com>
2025-03-21 17:49:39 +0200

    made a function

--- games/rock_paper_scissors.py
+++ games/rock_paper_scissors.py
@@ -1,3 +1,7 @@
-player_choice = "rock"
-computer_choice = "paper"
+# rock paper scissors game
 
+def get_choices():
+    player_choice = "rock"
+    computer_choice = "paper"
+
+    return player_choice
